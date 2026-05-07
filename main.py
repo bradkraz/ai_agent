ef6851efd432429119e08ff621e9739c2eb5f958
bradkraz <bradkraz@gmail.com>
2026-05-07 15:36:10 +0800

    add calculator

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from google.genai import types
 def main():
     parser = argparse.ArgumentParser(description="Chat Agent")
     parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -18,16 +18,32 @@ def main():
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
     client = genai.Client(api_key=api_key)
+    messages = [
+        types.Content(
+            role="user",
+            parts=[types.Part(text=args.user_prompt)],
+        )
+    ]
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
+
+    generate_content(client, messages, args.verbose)
+
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.5-flash", contents=messages
     )
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response:")
     print(response.text)
+
+    return response
 
 
 if __name__ == "__main__":
