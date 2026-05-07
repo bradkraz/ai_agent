6ee8079695ab9e0789f1e36afdb4b2adec3142d8
bradkraz <bradkraz@gmail.com>
2026-05-07 11:00:12 +0800

    add message parsing

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import os
+import argparse
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Chat Agent")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
@@ -12,8 +19,7 @@ def main():
 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        model="gemini-2.5-flash", contents=messages
     )
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
